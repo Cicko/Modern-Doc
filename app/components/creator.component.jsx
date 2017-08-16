60f1ccfdf252db4bf12146fe24da0285bb99135a
Cicko <rudolfcicko2016@gmail.com>
2017-08-16 22:52:45 +0100

    Working disabling some fields

--- app/components/creator.component.jsx
+++ app/components/creator.component.jsx
@@ -8,16 +8,31 @@ const types = ['Book','API','FAQ','Other']
 
 class Creator extends Component {
   state = {
-    type: types[0]
+    type: types[0],
+    isPrivate: false
   };
+
 
   handleChange = (e, {value}) =>  {
     this.setState({type: value})
+    if (value == types[3])
+      $('.template').removeClass('disabled')
+    else
+      $('.template').addClass('disabled')
+  }
+
+  handlePrivate = (e, {checked}) => {
+    this.setState({isPrivate: checked});
+    if (checked)
+      $('.private').removeClass('disabled')
+    else
+      $('.private').addClass('disabled')
   }
 
   handleSubmit = e => {
-    const { title, author, }
+    //const { title, author }
   }
+
 
   render() {
     const { type } = this.state;
@@ -34,8 +49,10 @@ class Creator extends Component {
           <Form.Field control={Radio} label={types[2]} value={types[2]} checked={type === types[2]} onChange={this.handleChange} />
           <Form.Field control={Radio} label={types[3]} value={types[3]} checked={type === types[3]} onChange={this.handleChange} />
         </Form.Group>
+        <Form.Field control={Input} className="template disabled" name="template" label='Template name' placeholder='template' />
         <Form.Field control={TextArea} label='About' placeholder='Tell us more about you...' />
-        <Form.Field control={Checkbox} label='I agree to the Terms and Conditions' />
+        <Form.Field control={Checkbox} label='Is Private' onChange={this.handlePrivate}/>
+        <Form.Field control={Input} className="private disabled" name="organization" label="Github's Organization name" placeholder='organization'/>
         <Form.Field control={Button}>Submit</Form.Field>
       </Form>
     )
